0af07a176d20352c3b6542011c9ff6b4623df727
will <wizarot@qq.com>
2020-05-05 12:17:16 +0800

    <新章节>: (Controller,Model) <学习插件lombok和自动热更新插件devtools>

--- src/main/java/com/wizarot/studyspringboot/controller/HelloController.java
+++ src/main/java/com/wizarot/studyspringboot/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.wizarot.studyspringboot.controller;
 
+import com.wizarot.studyspringboot.model.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
+
         return "Hello world! 中文你好.";
+    }
+
+    @RequestMapping("/article")
+    public Article article() {
+        Article art1 = Article.builder().author("wiz").id(1L).title("Java学习").build();
+//        Article art2 = Article.builder().author("字母哥1").id(2L).build();
+//        log.info("测试article:{}",art2);
+        return art1;
     }
 }
--- src/main/java/com/wizarot/studyspringboot/model/Article.java
+++ src/main/java/com/wizarot/studyspringboot/model/Article.java
@@ -1,4 +1,16 @@
 package com.wizarot.studyspringboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Article {
+    private Long id;
+    private String author;
+    private String title;
 }
